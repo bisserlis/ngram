638369b84406e80d0821b170b8d5fd27fe8105ae
Ben Isserlis <b@isserl.is>
2014-07-04 16:28:13 -0700

    Added basic command-line args with optparse-applicative
    Added dependency on optparse-applicative.
    Added configuration options to command-line arguments.

--- src/Main.hs
+++ src/Main.hs
@@ -1,1 +1,31 @@
-main = putStrLn "Usage: ngram"
+module Main where
+
+import Options.Applicative
+
+data Configuration = Configuration
+                   { optN :: Int
+                   , optI :: Int
+                   , optInput :: FilePath
+                   }
+
+configuration :: Parser Configuration
+configuration = Configuration
+            <$> option
+                ( short 'n'
+               <> metavar "N"
+               <> value 2
+               <> help ( "Derive N-grams (bigrams, trigrams, 4-grams...) "
+                      ++ "(Default: 2)" ) )
+            <*> option
+                ( short 'i'
+               <> metavar "I"
+               <> value 10
+               <> help "Number of sequences to generate (Default: 10)" )
+            <*> argument str (metavar "FILE")
+
+main = execParser opts >>= undefined
+    where
+        opts = info (helper <*> configuration)
+               ( fullDesc
+              <> progDesc "Generate I sequences from N-grams derived from FILE"
+              <> header "ngram - an n-gram based sequence generator" )
