bc29886729ef7989c09ea434b8c54bf597f5e6f5
Ben Isserlis <b@isserl.is>
2014-07-07 17:27:26 -0700

    Implemented Markov model training
    Added dependency on containers.
    Implemented model based on Maps.
    Implemented training functions, gave some thought to future
      parallelization.
    Stubbed out remaining necessary functions (types).
    Pre-emptively added dependency on random for sequence generation.

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,11 @@
+{-# LANGUAGE BangPatterns #-}
+
 module Main where
 
 import Options.Applicative
+import Data.List
+import Data.Map (Map, fromList, fromListWith, singleton, unionWith, unionsWith)
+import System.Random
 
 data Configuration = Configuration
                    { optN :: Int
@@ -23,9 +28,40 @@ configuration = Configuration
                <> help "Number of sequences to generate (Default: 10)" )
             <*> argument str (metavar "FILE")
 
-main = execParser opts >>= undefined
+main :: IO ()
+main = execParser opts >>= deriveSequences
     where
         opts = info (helper <*> configuration)
                ( fullDesc
               <> progDesc "Generate I sequences from N-grams derived from FILE"
               <> header "ngram - an n-gram based sequence generator" )
+
+deriveSequences :: Configuration -> IO ()
+deriveSequences (Configuration n i f) = do
+        corpus <- fmap lines (readFile f)
+        stdGen <- getStdGen
+        mapM_ putStrLn . take i . markov stdGen . trains n $ corpus
+        return ()
+
+type Model a = Map (Sequence a) (Map (Maybe a) Int)
+type Sequence a = [a]
+
+train :: (Ord a) => Int -> Sequence a -> Model a
+train n = fromListWith (unionWith (+))
+          . map (fmap (flip singleton 1))
+          . gramsOf n
+
+unify :: (Ord a) => [Model a] -> Model a
+unify = unionsWith (unionWith (+))
+
+trains :: (Ord a) => Int -> [Sequence a] -> Model a
+trains n = unify . map (train n)
+
+gramsOf :: Int -> Sequence a -> [(Sequence a, Maybe a)]
+gramsOf n as = zip prefixes mas
+    where prefixes = map (\(i, l) -> drop (i - n) l) (zip [0..] (inits as))
+          mas = map Just as ++ [Nothing]
+
+markov :: StdGen -> Model a -> [Sequence a]
+markov = undefined
+
